1cb7e002c3fb258439d5466eaf0d43be8098f138
nfrerebeau <nicolas.frerebeau@u-bordeaux-montaigne.fr>
2022-08-17 11:01:24 +0200

    Update package metadata

---
+++ R/kirke-package.R
@@ -0,0 +1,31 @@
+#' @details
+#' \tabular{ll}{
+#'  **Package:** \tab kirke \cr
+#'  **Type:** \tab Package \cr
+#'  **Version:** \tab 0.0.0.9000 \cr
+#'  **License:** \tab GPL-3 \cr
+#'  **Zenodo:** \tab  \cr
+#' }
+#'
+#' @author
+#' **Full list of authors and contributors** (alphabetic order)
+#'
+#' \tabular{ll}{
+#'  Nicolas Frerebeau \tab *Université Bordeaux Montaigne, France* \cr
+#' }
+#'
+#' **Package maintainer**
+#'
+#' Nicolas Frerebeau\cr
+#' \email{nicolas.frerebeau@@u-bordeaux-montaigne.fr}
+#'
+#' Archéosciences Bordeaux (UMR 6034)\cr
+#' Maison de l'Archéologie\cr
+#' Université Bordeaux Montaigne\cr
+#' 33607 Pessac cedex\cr
+#' France
+#' @name kirke-package
+#' @aliases kirke
+#' @docType package
+#' @keywords internal
+"_PACKAGE"
