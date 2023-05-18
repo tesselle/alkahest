88b7699ef2027666de0befdc85f328d13067915d
nfrerebeau <nicolas.frerebeau@u-bordeaux-montaigne.fr>
2023-05-18 10:42:02 +0200

    Force tests to be executed if in dev release

--- tests/tinytest.R
+++ tests/tinytest.R
@@ -1,5 +1,7 @@
+if (requireNamespace("tinytest", quietly = TRUE)) {
+  ## Force tests to be executed if in dev release
+  ## (which we define as having a sub-release)
+  NOT_CRAN <- length(unclass(packageVersion("alkahest"))[[1]]) == 4
 
-if ( requireNamespace("tinytest", quietly=TRUE) ){
-  tinytest::test_package("alkahest")
+  tinytest::test_package("alkahest", at_home = NOT_CRAN)
 }
-
